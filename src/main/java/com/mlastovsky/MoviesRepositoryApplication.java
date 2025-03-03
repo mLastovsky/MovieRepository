a2c91a9171e55b9165281056213c35dd4cdc64a3
mLastovsky <LastovskyTech@gmail.com>
2025-03-04 01:44:14 +0300

    initial commit

---
+++ src/main/java/com/mlastovsky/MoviesRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.mlastovsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesRepositoryApplication.class, args);
+    }
+
+}
