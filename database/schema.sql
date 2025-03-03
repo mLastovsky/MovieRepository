e017e1721aae2bdb72bc4557f223eaa1f1a24fe4
mLastovsky <LastovskyTech@gmail.com>
2025-03-04 01:59:58 +0300

    chore: declare db schema.sql script file

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Director (
+                          id SERIAL PRIMARY KEY,
+                          firstName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Movie (
+                       id SERIAL PRIMARY KEY,
+                       director_id INT REFERENCES Director(id) ON DELETE CASCADE,
+                       name VARCHAR(255) NOT NULL
+);
