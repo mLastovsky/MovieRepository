97ea341e4aa3befeac99c12c0eaa092e39117929
mLastovsky <LastovskyTech@gmail.com>
2025-03-04 02:02:20 +0300

    add: created movie model

---
+++ src/main/java/com/mlastovsky/model/Movie.java
@@ -0,0 +1,26 @@
+package com.mlastovsky.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "director_id", referencedColumnName = "id")
+    @ToString.Exclude
+    private Director director;
+
+}
