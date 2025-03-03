a79fd7b02f5d1ef4e22964b25efe630e719a328b
mLastovsky <LastovskyTech@gmail.com>
2025-03-04 02:17:44 +0300

    add: implements crud repository

---
+++ src/main/java/com/mlastovsky/repository/DirectorRepository.java
@@ -0,0 +1,10 @@
+package com.mlastovsky.repository;
+
+import com.mlastovsky.model.Director;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DirectorRepository extends CrudRepository<Director, Integer> {
+
+}
