e0ecd32a1f982172ae4ed4d1a75496313b0790e6
mLastovsky <LastovskyTech@gmail.com>
2025-03-06 15:47:43 +0300

    feat: added logs and proper implementation of deleteDirector method

--- src/main/java/com/mlastovsky/service/DirectorService.java
+++ src/main/java/com/mlastovsky/service/DirectorService.java
@@ -5,30 +5,36 @@ import com.mlastovsky.model.Director;
 import com.mlastovsky.repository.DirectorRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @AllArgsConstructor
+@Slf4j
 public class DirectorService {
 
     private final DirectorRepository directorRepository;
 
     public Iterable<Director> getAllDirectors() {
+        log.debug("fetching All Directors");
         return directorRepository.findAll();
     }
 
     public Director getDirectorById(Long id) throws DirectorNotFoundException {
+        log.debug("fetching Director with id {}", id);
         return directorRepository.findById(id)
                 .orElseThrow(DirectorNotFoundException::new);
     }
 
     public void addDirector(Director director) {
+        log.debug("adding Director with firstname {}", director.getFirstname());
         directorRepository.save(director);
     }
 
     public void deleteDirector(Long id) throws DirectorNotFoundException {
-        var director = directorRepository.findById(id);
-        director.ifPresent(directorRepository::delete);
+        log.debug("deleting Director with id {}", id);
+        var director = this.getDirectorById(id);
+        directorRepository.delete(director);
     }
 }
