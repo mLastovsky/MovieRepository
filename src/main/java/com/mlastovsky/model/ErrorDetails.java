c79694b1af83ad0cdfc848644529a22f769762e6
mLastovsky <LastovskyTech@gmail.com>
2025-03-06 15:13:11 +0300

    refactor: added AllArgsConstructor annotation

--- src/main/java/com/mlastovsky/model/ErrorDetails.java
+++ src/main/java/com/mlastovsky/model/ErrorDetails.java
@@ -1,10 +1,14 @@
 package com.mlastovsky.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ErrorDetails {
+
     private String error;
+
 }
