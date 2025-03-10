e9b900f6b3420cbd7fa2a5c3593961ea92ef4050
mLastovsky <LastovskyTech@gmail.com>
2025-03-10 15:20:08 +0300

    refactor: add in not exist in signature

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Director
+CREATE TABLE IF NOT EXISTS Director
 (
     id        SERIAL PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Movie
+CREATE TABLE IF NOT EXISTS Movie
 (
     id          SERIAL PRIMARY KEY,
     director_id INT REFERENCES Director (id) ON DELETE CASCADE,
